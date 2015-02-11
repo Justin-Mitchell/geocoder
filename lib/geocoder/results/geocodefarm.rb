c8f3312642be8b762a6f436eccfc449abdcadb1c
Justin-Mitchell <jmitchell4140@gmail.com>
2015-02-10 20:42:45 -0800

    result methods added

--- lib/geocoder/results/geocodefarm.rb
+++ lib/geocoder/results/geocodefarm.rb
@@ -4,19 +4,19 @@ module Geocoder::Result
   class Geocodefarm < Base
 
     def address
-        @data['ADDRESS']['address_returned']
+        @data['RESULTS'][0]['ADDRESS']['street_number'] + " " + @data['RESULTS'][0]['ADDRESS']['street_name'] + ", " + @data['RESULTS'][0]['ADDRESS']['locality'] + ", " + @data['RESULTS'][0]['ADDRESS']['admin_1'] + " " + @data['RESULTS'][0]['ADDRESS']['postal_code']
     end
 
     def address_provided
-        @data['ADDRESS']['address_provided']
+        @data['RESULTS'][0]['formatted_address']
     end
 
     def latitude
-        @data['COORDINATES']['latitude'].to_f
+        @data['RESULTS'][0]['COORDINATES']['latitude'].to_f
     end
 
     def longitude
-        @data['COORDINATES']['longitude'].to_f
+        @data['RESULTS'][0]['COORDINATES']['longitude'].to_f
     end
 
     def coordinates
@@ -24,12 +24,41 @@ module Geocoder::Result
     end
 
     def quality
-        @data['ADDRESS']['accuracy']
+        @data['RESULTS'][0]['accuracy']
     end
 
-    def self.response_attributes
-        %w[]
+    def address_components
+      @data['RESULTS'][0]['ADDRESS']
     end
+    
+    def state
+      @data['RESULTS'][0]['ADDRESS']['admin_1']
+    end
+
+    def country
+      @data['RESULTS'][0]['ADDRESS']['country']
+    end
+
+    def postal_code
+      @data['RESULTS'][0]['ADDRESS']['postal_code']
+    end
+
+    def route
+      @data['RESULTS'][0]['ADDRESS']['street_name']
+    end
+
+    def street_number
+      @data['RESULTS'][0]['ADDRESS']['street_number']
+    end
+
+    def street_address
+      [street_number, route].compact.join(' ')
+    end
+    
+    def boundaries
+        @data['RESULTS'][0]['BOUNDARIES']
+    end
+    
 
     response_attributes.each do |a|
       unless method_defined?(a)
